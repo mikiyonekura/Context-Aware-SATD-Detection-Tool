b3af8b2466b2f2d30e1fb8e88b20fcad65a5fbf3
Yonekura Miki <miki-yo@cc21dev0.naist.jp>
2023-10-18 21:51:42 +0900

    ROC

--- 1-encoder/predict_project.py
+++ 1-encoder/predict_project.py
@@ -3,14 +3,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 import torch
+import preprocess as pre
+from sklearn.metrics import roc_curve, auc
 
 def predict_satd(data, model, tokenizer):
     true_labels = []
     predicted_labels = []
+    predicted_probs = []  # 予測された確率を保存するためのリスト
 
     for d in data:
         text = d["comment"]
+        #変更点
+        # preprocessed_text = pre.stemmerLemmatizer(text)
+
         inputs = tokenizer(text, truncation=True, padding=True, return_tensors="pt")
+        #変更点
+        # inputs = tokenizer(preprocessed_text, truncation=True, padding=True, return_tensors="pt")
         
         # If available use GPU
         if torch.cuda.is_available():
@@ -28,6 +36,10 @@ def predict_satd(data, model, tokenizer):
         # Get probability for non-SATD and SATD
         non_satd_prob = probabilities[0][0].item()
         satd_prob = probabilities[0][1].item()
+
+        # Save the probability of SATD
+        predicted_probs.append(satd_prob)
+
 
         # Predicted label is the one with the highest probability
         predicted_label = 0 if non_satd_prob > satd_prob else 1
@@ -55,6 +67,32 @@ def predict_satd(data, model, tokenizer):
     plt.yticks([0.5,1.5], ['non-SATD', 'SATD'], rotation=90, va='center')
     plt.savefig("confusion_matrix.png")
     # plt.show()
+
+    # 2. ROC曲線のためのデータを取得
+    fpr, tpr, thresholds = roc_curve(true_labels, predicted_probs, pos_label=1)
+    roc_auc = auc(fpr, tpr)
+    #true_labels, predicted_probsをprint
+    print(true_labels)
+    print(predicted_probs)
+    #txtとして保存
+    with open('true_labels-1.txt', 'w') as f:
+        for item in true_labels:
+            f.write("%s\n" % item)
+    
+    with open('predicted_prob-1.txt', 'w') as f:
+        for item in predicted_probs:
+            f.write("%s\n" % item)
+
+    # 3. ROC曲線をプロット
+    plt.figure(figsize=(8, 6))
+    plt.plot(fpr, tpr, color='darkorange', label=f'ROC curve (area = {roc_auc:.2f})')
+    plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc="lower right")
+    plt.savefig("roc_curve.png")
+    plt.show()
 
 if __name__ == "__main__":
     predict_satd(data, model, tokenizer)
---
+++ extra/roc.py
@@ -0,0 +1,37 @@
+import matplotlib
+matplotlib.use('Agg')  # バックエンドを指定
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+
+# ファイルからのデータを読み込み、数値型に変換
+with open('/work/miki-yo/true_labels-1.txt', 'r') as f:
+    true_labels = [int(line.strip()) for line in f.readlines()]  # int型に変換
+
+with open('/work/miki-yo/predicted_probs-1.txt', 'r') as f:
+    predicted_probs = [float(line.strip()) for line in f.readlines()]  # float型に変換
+
+with open('/work/miki-yo/true_labels-2.txt', 'r') as f:
+    true_labels2 = [int(line.strip()) for line in f.readlines()]  # int型に変換
+
+with open('/work/miki-yo/predicted_probs-2.txt', 'r') as f:
+    predicted_probs2 = [float(line.strip()) for line in f.readlines()]  # float型に変換
+
+#ROC曲線を描画
+fpr, tpr, thresholds = roc_curve(true_labels, predicted_probs, pos_label=1)
+roc_auc = auc(fpr, tpr)
+fpr2, tpr2, thresholds2 = roc_curve(true_labels2, predicted_probs2, pos_label=1)
+roc_auc2 = auc(fpr2, tpr2)
+plt.figure(figsize=(6, 5))
+plt.plot(fpr, tpr, label='1-input (area = %.2f)'%roc_auc)
+plt.plot(fpr2, tpr2, label='2-input (area = %.2f)'%roc_auc2)
+plt.legend()
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.savefig("roc_curveeeee.png")
+plt.show()
+
+
+
+
+
